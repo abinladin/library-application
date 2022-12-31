10e213ad4c822e6e2a053c11e83449b282b047eb
abinladin <abdullahbinladin98@gmail.com>
2022-12-31 02:09:29 +0100

    update to previously held files

---
+++ src/main/java/abinladin/libraryapplication/BookController.java
@@ -0,0 +1,69 @@
+package abinladin.libraryapplication;
+
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+
+public class BookController {
+
+    private ArrayList<Book> bookArrayList = new ArrayList<Book>();
+    
+    public TextField titleField;
+    public TextField authorField;
+    public DatePicker publicationDate;
+    public TextField publisher;
+    public TextField edition;
+    public TextField ISBN;
+
+    public Button submit;
+
+
+    @FXML
+    private void buttonAddBook(){
+
+        Book book = new Book();
+
+        book.setTitle(titleField.getText());
+        System.out.println("book: " + book.getTitle());
+
+        book.setAuthor(authorField.getText());
+        System.out.println("author: " + book.getAuthor());
+
+        book.setPublicationDate(publicationDate.getValue());
+        System.out.println("PublicationDate: " + book.getPublicationDate());
+
+        book.setPublisher( publisher.getText());
+        System.out.println("Publisher: " + book.getPublisher());
+
+        book.setEdition(edition.getText());
+        System.out.println("Edition: " + book.getEdition());
+
+        bookArrayList.add(book);
+
+        titleField.clear();
+        authorField.clear();
+        publisher.clear();
+        edition.clear();
+        publicationDate.setValue(null);
+        publicationDate.setPromptText("Publication Date");
+
+    }
+
+    @FXML
+    private void printStoredBooks(){
+        int i = 1;
+        System.out.println("=====================================");
+        System.out.println("           BOOKS IN DATABASE         ");
+        System.out.println("=====================================");
+        for (Book e: bookArrayList) {
+            System.out.println("Entry: " + i++);
+            System.out.println(e);
+        }
+    }
+
+
+}
--- src/main/java/abinladin/libraryapplication/MainApplication.java
+++ src/main/java/abinladin/libraryapplication/MainApplication.java
@@ -7,13 +7,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello, World!!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("add-book-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        stage.setTitle("IUG Library Application App");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
